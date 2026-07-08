4381a2371dae4b87204ede32544af95911bc7c01
ReubenZSTechs <reuben.zachary.rz@gmail.com>
2026-07-08 21:22:23 +0700

    Dockerized gemma4-E2B

--- training/scripts/quantize_model.py
+++ training/scripts/quantize_model.py
@@ -18,18 +18,31 @@ def quantize_model(model_name: str, quantized_name: str):
     quant_path = Path(os.getenv("HUGGINGFACE_QUANTIZE_FOLDERPATH")).expanduser() / quantized_name
     quant_path.mkdir(exist_ok=True, parents=True)
 
+    offload_path = Path(os.getenv("HUGGINGFACE_QUANTIZE_FOLDERPATH")).expanduser() / "offload_tmp"
+    offload_path.mkdir(exist_ok=True, parents=True)
+
     print(f"Loading {model_name}...")
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto", dtype=torch.bfloat16, max_memory={0: "13GiB", "cpu": "64GiB"})
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, 
+        device_map={"":0}, 
+        dtype=torch.bfloat16, 
+        # max_memory={0: "13GiB", "disk": "20GiB"},
+        low_cpu_mem_usage=True,
+        # offload_folder=str(offload_path),
+        # offload_state_dict=True
+        )
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     print(f"Loading calibration dataset")
     quant_config = GPTQModifier(
     scheme="W4A16",
     targets="Linear",
+    offload_hessians=True,
     ignore=[
         'lm_head',
         're:.*vision_tower.*',
         're:.*audio_tower.*',
+        "re:.*visual.*",
         're:.*per_layer_model_projection.*',
         're:.*per_layer_.*',       # catches related per-layer embedding projections if present
         're:.*altup.*',            # Gemma 3n/4 AltUp modules, if present, are also often unsuitable for GPTQ
@@ -40,11 +53,11 @@ def quantize_model(model_name: str, quantized_name: str):
     oneshot(
         model=model,
         dataset="ultrachat_200k",
-        splits="train_sft+train_gen+test_gen",
+        splits="train_sft[:64]",
         recipe=quant_config,
         output_dir=str(quant_path),
-        max_seq_length=512,
-        num_calibration_samples=128,
+        max_seq_length=256,
+        num_calibration_samples=16,
         pipeline="basic"
     )
 
